bc4bf2f3ecfea9a0e7c4b8976938dc667a421cb9
BineeshaBaby <125890221+BineeshaBaby@users.noreply.github.com>
2023-04-24 23:12:10 +0530

    Add files via upload
    functions added

--- 22023287.py
+++ 22023287.py
@@ -7,51 +7,87 @@ Created on Mon Apr 24 20:44:48 2023
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import percentileofscore
 
-# Read in the data from the CSV file using Pandas, specifying that there is no header
-data = pd.read_csv("D:/data7.csv", header=None)
+def read_newborn_data(file_path):
+    """
+    Reads data from a CSV file without headers and returns a Pandas DataFrame object.
+
+    Args:
+    file_path (str): The path to the CSV file.
+
+    Returns:
+    Pandas DataFrame object: The data read from the CSV file.
+    """
+    return pd.read_csv(file_path, header=None)
+
+def calculate_histogram(data_column):
+    """
+    Calculates the histogram of the given data column using the Freedman-Diaconis rule.
+
+    Args:
+    data_column (Pandas Series object): The data column to calculate the histogram for.
+
+    Returns:
+    Tuple: A tuple containing the histogram counts and bin edges.
+    """
+    q1, q3 = np.percentile(data_column, [25, 75])
+    iqr = q3 - q1
+    bin_width = 2 * iqr / len(data_column) ** (1 / 3)
+    bin_range = np.arange(np.min(data_column), np.max(data_column) + bin_width, bin_width)
+    hist, bin_edges = np.histogram(data_column, bins=bin_range)
+    return hist, bin_edges
+
+def plot_histogram(hist, bin_edges, weights_mean, x_val):
+    """
+    Plots the histogram of the data, with vertical lines for the mean weight and the X value.
+
+    Args:
+    hist (numpy array): The histogram counts.
+    bin_edges (numpy array): The bin edges for the histogram.
+    weights_mean (float): The mean weight of the data.
+    x_val (float): The value of X such that 75% of newborns from the distribution are born with a weight below X.
+
+    Returns:
+    None
+    """
+    legend_label = 'W_tilde = {:.2f}, X = {:.2f}'.format(weights_mean, x_val)
+    plt.hist(weights, bins=bin_edges, label=legend_label)
+    plt.xlabel("Newborn weight")
+    plt.ylabel("Count")
+    plt.title("Distribution of newborn weights", fontweight="bold") # Make title bold
+    plt.axvline(weights_mean, color='r', linestyle='--', label=r'$\tilde{W}$')
+    plt.axvline(x_val, color='g', linestyle='--', label='X')
+    plt.legend(loc='upper left', bbox_to_anchor=(0.7, 0.9))
+    plt.show()
+
+# Read in the data from the CSV file
+data = read_newborn_data("D:/data7.csv")
+print(data)
 
 # Extract the column of data containing the newborn weights
 weights = data.iloc[:, 0]
 
-# Use the Freedman-Diaconis rule to calculate the optimal number of bins for the histogram
-q1, q3 = np.percentile(weights, [25, 75])
-iqr = q3 - q1
-bin_width = 2 * iqr / len(weights) ** (1 / 3)
-n_bins = int((np.max(weights) - np.min(weights)) / bin_width)
+# Calculate the histogram of the weights column
+hist, bin_edges = calculate_histogram(weights)
 
-# Define the range of the histogram bins
-bin_range = np.arange(np.min(weights), np.max(weights) + bin_width, bin_width)
-
-# Create a histogram of the weights using the defined bins
-hist, bin_edges = np.histogram(weights, bins=bin_range)
-
-# Print the histogram and the bin edges
-print("Histogram:", hist)
-print("Bin edges:", bin_edges)
-
-# Assume that the weights array contains the weights of newborn babies in the given region
-W_tilde = np.mean(weights)
+# Calculate the mean weight
+weights_mean = np.mean(weights)
+print("Mean weight:", weights_mean)
 
 # Calculate the value of X such that 75% of newborns from the distribution are born with a weight below X
-X = np.percentile(weights, 75)
+x_val = np.percentile(weights, 75)
 
-# Define the label for the legend
-legend_label = 'W_tilde = {:.2f}, X = {:.2f}'.format(W_tilde, X)
+# Print the value of X
+print("The value of X is:", x_val)
+
+# Calculate the percentage of newborns with a weight below X
+percent_below_X = 100 - percentileofscore(weights, x_val)
+
+# Print the result
+print("{:.2f}% of newborns are born with a weight below {:.2f}.".format(percent_below_X, x_val))
+
 
 # Plot the histogram with labels and legend
-plt.hist(weights, bins=bin_edges, label=legend_label)
-plt.xlabel("Newborn weight")
-plt.ylabel("Count")
-plt.title("Distribution of newborn weights")
-
-# Add vertical lines for W_tilde and X
-plt.axvline(W_tilde, color='r', linestyle='--', label=r'$\tilde{W}$')
-plt.axvline(X, color='g', linestyle='--', label='X')
-
-# Create the legend outside of the plot box
-plt.legend(loc='upper left', bbox_to_anchor=(0.7, 0.9))
-
-# Display the plot
-plt.show()
+plot_histogram(hist, bin_edges, weights_mean, x_val)
 
